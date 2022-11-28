d7c9ddac6b2898a50dacfef39506909991e27cbf
teofiln <teofiln@gmail.com>
2022-11-28 13:02:03 -0600

    basic draft ok.

--- SemejnoNasilstvoDash/R/app_server.R
+++ SemejnoNasilstvoDash/R/app_server.R
@@ -5,4 +5,21 @@
 #' @import shiny
 #' @noRd
 app_server <- function(input, output, session) {
+
+  output$crime_table <- DT::renderDT(rownames = FALSE, server = TRUE, filter = "top", {
+    SemejnoNasilstvoData:::crimes_mk
+  })
+
+  output$victims_table <- DT::renderDT(rownames = FALSE, server = TRUE, filter = "top", {
+    SemejnoNasilstvoData:::victims_mk
+  })
+
+  output$misdemeanors_table <- DT::renderDT(rownames = FALSE, server = TRUE, filter = "top", {
+    SemejnoNasilstvoData:::misdemeanors_mk
+  })
+
+  output$complaints_table <- DT::renderDT(rownames = FALSE, server = TRUE, filter = "top", {
+    SemejnoNasilstvoData:::complaints_mk
+  })
+
 }
--- SemejnoNasilstvoDash/R/app_ui.R
+++ SemejnoNasilstvoDash/R/app_ui.R
@@ -7,8 +7,51 @@
 app_ui <- function(request) {
   tagList(
     golem_add_external_resources(),
-    fluidPage(
-      h1("SemejnoNasilstvoDash")
+    shiny::div(
+      class = "container-xl",
+      bslib::page_fluid(
+        theme = bslib::bs_theme(version = 5, bootswatch = "lux"),
+        shiny::div(
+          class = "text-center",
+          shiny::h1("Семејно Насилство"),
+          shiny::br(),
+          shiny::br(),
+          shiny::div(
+            class = "container-xl",
+            bslib::navs_pill_card(
+              bslib::nav(
+                "Криминални случаи",
+                shiny::br(),
+                shiny::br(),
+                DT::DTOutput("crime_table")
+              ),
+              bslib::nav(
+                "Сторители и жртви",
+                shiny::br(),
+                shiny::br(),
+                DT::DTOutput("victims_table")
+              ),
+              bslib::nav(
+                "Прекршоци",
+                shiny::br(),
+                shiny::br(),
+                DT::DTOutput("misdemeanors_table")
+              ),
+              bslib::nav(
+                "Поплаки",
+                shiny::br(),
+                shiny::br(),
+                DT::DTOutput("complaints_table")
+              ),
+              bslib::nav(
+                "За податоците",
+                shiny::br(),
+                shiny::br()
+              )
+            )
+          )
+        )
+      )
     )
   )
 }
